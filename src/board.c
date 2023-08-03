63e11134d8dace8e2585993524f006bdfa11da64
atamakahere <git@atamaka.maskmy.id>
2023-08-03 18:45:20 +0530

    fix(doc): board layout in comments

--- src/board.c
+++ src/board.c
@@ -9,8 +9,9 @@
  *    3|  41  42  43  44  45  46  47  48
  *    4|  51  52  53  54  55  56  57  58
  *    5|  61  62  63  64  65  66  67  68
- *    6|  71  72  73  74  75  76  77  78 7|  81  82  83  84  85  86  87  88 8|
- * 91  92  93  94  95  96  97  98
+ *    6|  71  72  73  74  75  76  77  78
+ *    7|  81  82  83  84  85  86  87  88
+ *    8|  91  92  93  94  95  96  97  98
  *
  * The board representation ranges between 0-119, the above shown positions
  * represents position on board while others are overflow. All pieces after one
